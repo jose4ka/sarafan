bba0001ae1cbeaa3b71315d720306ef7b83a9fae
loakdv <rabowvdk125@gmail.com>
2020-08-01 22:11:07 +1000

    Connected Dtabase

---
+++ src/main/java/com/loakdv/sarafan/domain/Views.java
@@ -0,0 +1,7 @@
+package com.loakdv.sarafan.domain;
+
+public final class Views {
+    public interface IdName {}
+
+    public interface FullMessage extends  IdName{}
+}
---
+++ src/main/java/com/loakdv/sarafan/repo/MessageRepo.java
@@ -0,0 +1,7 @@
+package com.loakdv.sarafan.repo;
+
+import com.loakdv.sarafan.domain.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageRepo extends JpaRepository<Message, Long> {
+}
